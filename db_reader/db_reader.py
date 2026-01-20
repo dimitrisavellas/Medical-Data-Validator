19d4facdcdd0d4f5021e7b80ce93da163ab498af
Dimitrios Avellas <dimitrisavellas8@gmail.com>
2026-01-20 15:16:16 +0100

    Fix typo in sqlite3 connection method

--- db_reader/db_reader.py
+++ db_reader/db_reader.py
@@ -1,6 +1,6 @@
 import sqlite3
 import pandas as pd
-conn = sqlite3.connecSt('logs/audit_trail.db')
+conn = sqlite3.connect('logs/audit_trail.db')
 df = pd.read_sql_query("SELECT * FROM audit_log", conn)
 print(df)
 conn.close()
